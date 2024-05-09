3888cef934db8ecc0abeddcbb3846caf9e1876f4
Fi-Boo <s2008156@student.rmit.edu.au>
2024-05-09 23:52:21 +1000

    admin page mostly complete. Just need to adjust it so staff can edit their own name and password

--- Application.py
+++ Application.py
@@ -50,7 +50,85 @@ def getStaffLevel(email):
     )
     
     return response['Item']['staff_level']
+
+def checkExists(email):
+    
+    table_name = "cca3-login"
+    table = dynamodb.Table(table_name)
+    
+    response = table.get_item(
+        Key={
+            'email': email
+        }
+    )
+    
+    if 'Item' in response:
+        return True
+    return False
         
+def addNewUser(email, username, password, staffLevel):
+    
+    table_name = "cca3-login"
+    table = dynamodb.Table(table_name)
+    
+    item = {
+        'email': email,
+        'user_name': username,
+        'password': password,
+        'staff_level': staffLevel
+    }
+    
+    table.put_item(Item=item)
+    
+
+def getUsersList():
+    
+    table_name = "cca3-login"
+    table = dynamodb.Table(table_name)
+    response = table.scan()
+    users = []
+    
+    for item in response['Items']:
+        user = {
+            'email': item['email'],
+            'name': item['user_name'],
+            'password': item['password'],
+            'staffLevel': item['staff_level']
+        }
+        users.append(user)
+    
+    return users
+
+def editUser(email, name, staffLvl):
+    
+    table_name = "cca3-login"
+    table = dynamodb.Table(table_name)
+    
+    response = table.get_item(
+        Key={
+            'email': email
+        }
+    )
+    
+    item = response['Item']
+    user = {
+        'email': item['email'],
+        'user_name': name,
+        'password': item['password'],
+        'staff_level': staffLvl
+    }
+    
+    table.put_item(Item=user)
+    
+    
+def deleteUser(email):
+
+    table_name = "cca3-login"
+    table = dynamodb.Table(table_name)
+    key = {'email': email}
+    
+    response = table.delete_item(Key=key)
+    
 
 application = Flask(__name__)
 
@@ -65,7 +143,7 @@ def login():
     
     error = None
     if request.method == 'POST':
-        email = request.form['email']
+        email = request.form['email'].lower()
         password = request.form['password']
         
         if validateLogin(email, password) == True:
@@ -97,9 +175,76 @@ def main():
 
 @application.route('/admin', methods=["GET", "POST"])
 def admin():
-    return render_template('admin.html', staffLevel = session['staffLevel'])
+    
+    if 'loggedUser' in session:
+        
+        error = request.args.get('m')
+        
+        users = getUsersList()
+        
+        return render_template('admin.html', staffLevel = session['staffLevel'], users = users, error = error)
+    
+    else:
+        return redirect(url_for('login'))
+
+@application.route('/addUser', methods=["GET", "POST"])
+def addUser():
+    
+    if 'loggedUser' in session:
+        
+        if request.method == 'POST':
+
+            email = request.form['addUserEmail'].lower()
+            username = request.form['addUserName']
+            password = request.form['addUserPassword']
+            passwordconf = request.form['addUserPasswordConfirm']
+            staffLevel = request.form['addUserStaffLvl']
+            
+            if checkExists(email) == True:
+                
+                m = "Email already exists."
+
+            elif password != passwordconf:
+                
+                m = "Password mismatch."
+
+            else:
+                
+                addNewUser(email, username, password, staffLevel)
+                m = "New user added"
+
+            return redirect(url_for('admin', m = m))
+                 
+    else:
+        return redirect(url_for('login'))
 
 
+@application.route('/admin_process', methods=["GET", "POST"])
+def adminProcess():
+    
+    if 'loggedUser' in session:
+        
+        if request.method == 'POST':
+            
+            action = request.form['action']
+            email = request.form['editUserEmail']
+            name = request.form['editUserName']
+            staffLvl = request.form['editUserLvl']
+            m = None
+            
+            if action == 'edit':
+                
+                editUser(email, name, staffLvl)
+                m = "Changes to staff successful"
+                
+            elif action == 'delete':
+                
+                deleteUser(email)
+                m = "Staff removed successful"
+
+            return redirect(url_for('admin', m = m))
+    else:
+        return redirect(url_for('login'))
 
 if __name__ == "__main__":
     #application.debug = True
